7e0f44581a9432ecd0260a01d77c1f366b3d0716
Agus Tiyan <agustiyaninfo@gmail.com>
2021-02-26 16:01:47 +0700

    Create TableViewCell

---
+++ BeautifulIndonesia/PlaceTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PlaceTableViewCell.swift
+//  BeautifulIndonesia
+//
+//  Created by Agus Tiyansyah Syam on 26/02/21.
+//
+
+import UIKit
+
+class PlaceTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
