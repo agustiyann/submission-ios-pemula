4b088f830633cfa5f0ccc85b46f1efa3f965769a
Agus Tiyan <agustiyaninfo@gmail.com>
2021-02-26 15:37:12 +0700

    Initial Commit

---
+++ BeautifulIndonesia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeautifulIndonesia
+//
+//  Created by Agus Tiyansyah Syam on 26/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
